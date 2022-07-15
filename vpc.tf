c29796b8eafdb19689f37f2401b68568019512d7
sramika <sramika1990@gmail.com>
2022-07-15 13:22:39 +0530

    newcommit

--- vpc.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_vpc" "terraform-2ec2" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "terra-vpc"
-    Purpose = "Jenkins terra"
-  }
-}
