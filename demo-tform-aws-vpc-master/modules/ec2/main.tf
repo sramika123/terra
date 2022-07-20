fb9b570aca52de7d02e198e4afc6588a20ff8513
sramika123 <109295352+sramika123@users.noreply.github.com>
2022-07-20 16:31:24 +0530

    Update main.tf

--- demo-tform-aws-vpc-master/modules/ec2/main.tf
+++ demo-tform-aws-vpc-master/modules/ec2/main.tf
@@ -13,15 +13,13 @@ data "aws_ami" "amazon-linux-2" {
 
 // Configure the EC2 instance in a public subnet
 resource "aws_instance" "ec2_public" {
- 
-  ami                         = data.aws_ami.amazon-linux-2.id
+   ami                         = data.aws_ami.amazon-linux-2.id
   associate_public_ip_address = true
   instance_type               = "t2.micro"
   key_name                    = var.key_name
   subnet_id                   = var.vpc.public_subnets[0]
   vpc_security_group_ids      = [var.sg_pub_id]
   tags = {
-    count=2
     "Name" = "${var.namespace}-EC2-PUBLIC"
   }
 
@@ -54,7 +52,10 @@ resource "aws_instance" "ec2_public" {
 }
 
 // Configure the EC2 instance in a private subnet
-resource "aws_instance" "ec2_private" {
+resource "aws_instance" "ec2_private" { 
+  count =  2  
+  name = "server-${count.index}"
+ 
   ami                         = data.aws_ami.amazon-linux-2.id
   associate_public_ip_address = false
   instance_type               = "t2.micro"
